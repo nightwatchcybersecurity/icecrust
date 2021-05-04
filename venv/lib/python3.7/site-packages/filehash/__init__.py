9184bf9a29a35fd3e321e106096bb6375c1e0a3d
nightwatchcyber <research@nightwatchcybersecurity.com>
2021-05-04 17:17:30 -0400

    removed venv

--- venv/lib/python3.7/site-packages/filehash/__init__.py
+++
@@ -1,6 +0,0 @@
-try:
-    from filehash import FileHash, SUPPORTED_ALGORITHMS
-except ImportError:
-    from .filehash import FileHash, SUPPORTED_ALGORITHMS
-
-__all__ = ["FileHash", "SUPPORTED_ALGORITHMS"]
